01fad1f85a747765b662db6cd2d36e2c249125ad
NandoPla <fernandopla8@gmail.com>
2022-01-11 22:36:30 -0300

    Feat: formulario reativo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
-  return (
+
+  const [digita, setDigita] = useState(""); // digita é o valor digitado do input, setDigita é o valor que se atualiza constantemente
+  const handleChange = (event) => { // é a função que muda de acordo com o que digita, é o evento que pega tudo o que a pessoa digita
+    setDigita(event.target.value) 
+}
+
+  return ( //value é o parametro
     <>
-    </>
+    <form>
+      <label>Coloque seu nome: 
+        <input type="text" value={digita} onChange={handleChange}/>
+      </label>
+    </form>
+  
+  <div>{digita}</div>
+
+  </>
   );
 }
 
